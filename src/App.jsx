1fc866f6bec7f699857fa8d204aa08f6a55a9504
Koustubh-lapate <koustubh.lapate@gmail.com>
2024-02-15 16:41:31 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
+import Signin from "./Signin";
+import Appbar from "./Appbar";
 
 function App(){
     return(
@@ -6,7 +9,17 @@ function App(){
         height: "100vh",
         backgroundColor: "#eeeeee"}}>
 
-            
+            <Router>
+
+                <Appbar />
+
+                <Routes>
+                    <Route path="/login" element={<Signin />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/menu" element={<Menu />} />
+                </Routes>
+
+            </Router>
 
         </div>
     )  
