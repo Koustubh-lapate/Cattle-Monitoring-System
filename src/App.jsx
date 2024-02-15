3469a4a5fa87f93673a17836260552d2712b2bce
Koustubh-lapate <koustubh.lapate@gmail.com>
2024-02-15 16:34:11 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/Appbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+
+
 function App(){
-  
+    return(
+        <div style={{width: "100vw",
+        height: "100vh",
+        backgroundColor: "#eeeeee"}}>
+
+            
+
+        </div>
+    )  
 }
 
 export default App;
