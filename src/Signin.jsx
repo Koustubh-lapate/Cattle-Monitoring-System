8472f072a19bd8059b8aeed6fd9e6f759bc2b77b
Koustubh-lapate <koustubh.lapate@gmail.com>
2024-02-23 11:56:34 +0530

    modified:   src/Signin.jsx

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -1,5 +1,91 @@
-function Signin(){
+import { Button, Card, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
+function Signin(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return(
+        <div>
+
+            <div style={{
+                paddingTop: 100,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center"
+            }}>
+
+                <Typography variant="h6">
+                    <b>Welcome Back! Sign In below</b>
+                </Typography>
+
+            </div>
+
+            <div style={{display: "flex", justifyContent: "center"}}>
+
+                <Card variant="outlined" style={{
+                    width: 400,
+                    padding: 20,
+                }}>
+
+                    <TextField
+                    fullWidth={true}
+                    id="outlined-basic"
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+
+                    label="Enter Your Email"
+                    variant="outlined" />
+
+                    <br/><br/>
+
+                    <TextField
+                    fullWidth={true}
+                    id="outlined-basic"
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+
+                    label="Enter Password"
+                    variant="outlined"
+                    type="password" />
+
+                    <br/><br/>
+
+                    <Button
+                    size="large"
+                    variant="contained"
+
+                    onClick={() => {
+                        fetch("http://localhost:3000/user/login", {
+                            method: "POST",
+
+                            body: JSON.stringify({
+                                "useremail": email,
+                                "password": password,
+                            }),
+
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+
+                        }).then((resp) => {
+                            return resp.json().then((data) => {
+                                localStorage.setItem("token", data.token);
+                                window.location = "/menu";
+                            })
+                        })
+                    }}
+
+                    >Sign In</Button>
+
+                </Card>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Signin;
